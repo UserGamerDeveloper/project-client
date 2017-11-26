22dcda56b99b75ca70883f79b872d06c44adee71
Skatt <lampbox.company@gmail.com>
2017-11-26 17:01:42 +0400

    First commit

---
+++ app/src/main/java/com/example/skatt/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.skatt.myapplication;
+
+import com.example.skatt.myapplication.TopSheetBehavior;
+
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.deck_player);
+
+/*        ConstraintLayout llBottomSheet = (ConstraintLayout) findViewById(R.id.bottom_sheet);
+        TopSheetBehavior bottomSheetBehavior = TopSheetBehavior.from(llBottomSheet);
+
+        bottomSheetBehavior.setTopSheetCallback(new TopSheetBehavior.TopSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });*/
+    }
+}
