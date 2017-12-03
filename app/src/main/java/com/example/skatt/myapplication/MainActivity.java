e7ca58b3c4416da69812ca4df99eba40d96fc0cd
Skatt <lampbox.company@gmail.com>
2017-12-03 17:57:12 +0400

    тест анимации успешно завершен

--- app/src/main/java/com/example/skatt/myapplication/MainActivity.java
+++ app/src/main/java/com/example/skatt/myapplication/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.skatt.myapplication;
 
 import com.example.skatt.myapplication.TopSheetBehavior;
 
+import android.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +22,19 @@ public class MainActivity extends AppCompatActivity {
     int money;
     int hp;
     Card cards;
+    boolean mShowingBack = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.layout, new CardFrontFragment())
+                    .commit();
+        }
 
 /*        ConstraintLayout llBottomSheet = (ConstraintLayout) findViewById(R.id.bottom_sheet);
         TopSheetBehavior bottomSheetBehavior = TopSheetBehavior.from(llBottomSheet);
@@ -34,5 +50,48 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });*/
+    }
+
+    public void Flip(View view) {
+        if (mShowingBack) {
+
+            mShowingBack = false;
+
+            getFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(
+                            R.animator.card_flip_left_in,
+                            R.animator.card_flip_left_out)
+                    .replace(R.id.layout, new CardFrontFragment())
+                    .commit();
+        }
+        else{
+
+            // Flip to the back.
+
+            mShowingBack = true;
+
+            // Create and commit a new fragment transaction that adds the fragment for
+            // the back of the card, uses custom animations, and is part of the fragment
+            // manager's back stack.
+            getFragmentManager()
+                    .beginTransaction()
+
+                    // Replace the default fragment animations with animator resources
+                    // representing rotations when switching to the back of the card, as
+                    // well as animator resources representing rotations when flipping
+                    // back to the front (e.g. when the system Back button is pressed).
+                    .setCustomAnimations(
+                            R.animator.card_flip_right_in,
+                            R.animator.card_flip_right_out)
+
+                    // Replace any fragments currently in the container view with a
+                    // fragment representing the next page (indicated by the
+                    // just-incremented currentPage variable).
+                    .replace(R.id.layout, new CardBackFragment())
+
+                    // Commit the transaction.
+                    .commit();
+        }
     }
 }
