fd1fc7a2b625426df94ebe9a6869a8c38d0dc70a
Skatt <lampbox.company@gmail.com>
2017-12-06 18:26:23 +0400

    добавлен текст хп и дамаг у карт

--- app/src/main/java/com/example/skatt/myapplication/Card.java
+++ app/src/main/java/com/example/skatt/myapplication/Card.java
@@ -9,5 +9,4 @@ public class Card {
     int image;
     ImageView imageView;
 
-
 }
--- app/src/main/java/com/example/skatt/myapplication/MainActivity.java
+++ app/src/main/java/com/example/skatt/myapplication/MainActivity.java
@@ -1,40 +1,53 @@
 package com.example.skatt.myapplication;
 
-import com.example.skatt.myapplication.TopSheetBehavior;
-
-import android.app.Fragment;
-import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int money;
     int hp;
-    Card cards;
+    Card[] cards;
     boolean mShowingBack = false;
+    ImageView hp_bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        hp_bar = findViewById(R.id.hp_polosa);
+        hp_bar.getDrawable().setLevel(10000);
+
+        cards = new Card[9];
+        for (int i = 0; i<9;i++){
+            cards[i] = new Card();
+        }
+
+        cards[0].imageView = findViewById(R.id.card_view_1);
+        cards[1].imageView = findViewById(R.id.card_view_2);
+        cards[2].imageView = findViewById(R.id.card_view_3);
+        cards[3].imageView = findViewById(R.id.card_view_4);
+        cards[4].imageView = findViewById(R.id.card_view_5);
+        cards[5].imageView = findViewById(R.id.card_view_6);
+        cards[6].imageView = findViewById(R.id.card_view_7);
+        cards[7].imageView = findViewById(R.id.card_view_8);
+        cards[8].imageView = findViewById(R.id.card_view_9);
+
+/*
         if (savedInstanceState == null) {
             getFragmentManager()
                     .beginTransaction()
                     .add(R.id.layout, new CardFrontFragment())
                     .commit();
         }
+*/
 
 /*        ConstraintLayout llBottomSheet = (ConstraintLayout) findViewById(R.id.bottom_sheet);
         TopSheetBehavior bottomSheetBehavior = TopSheetBehavior.from(llBottomSheet);
@@ -50,6 +63,20 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });*/
+    }
+
+    public void onCardClick(View view){
+        int[] a = new int[2];
+        cards[0].imageView.getLocationOnScreen(a);
+        int[] b = new int[2];
+        cards[1].imageView.getLocationOnScreen(b);
+
+        TranslateAnimation anim = new TranslateAnimation(0, 0, 0, -500);
+        anim.setDuration(6000);
+        anim.setFillAfter(true);
+        ConstraintLayout constraintLayout1 = findViewById(R.id.card_table_1);
+        ConstraintLayout constraintLayout2 = findViewById(R.id.card_table_2);
+        constraintLayout1.setY(constraintLayout1.getY()-500);
     }
 
     public void Flip(View view) {
